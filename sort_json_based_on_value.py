17136d97f4b4b269e881bf5148da39d675bd6e76
rcvundavalli <95707181+rcvundavalli@users.noreply.github.com>
2022-04-06 21:49:12 -0400

    Update sort_json_based_on_value.py

--- sort_json_based_on_value.py
+++ sort_json_based_on_value.py
@@ -10,3 +10,39 @@ products.sort(key=lambda x: x["brand"])
 
 # Print the sorted JSON data
 print("The sorted JSON data based on the value of the brand:\n{0}".format(products))
+
+
+"""
+# import pprint module
+import pprint
+
+# Array of JSON Objects
+employee_list = [
+{
+    'name': 'firoz shah',
+    'post': 'HR',
+    'email': 'Accountant'
+},
+{
+    'name': 'Aiyan hasan',
+    'post': 'Sales',
+    'email': 'Manager'
+},
+{
+    'name': 'Mahmuda Feroz',
+    'post': 'Marketing',
+    'email': 'CEO'
+}]
+
+# Print the original JSON list
+print("Array of JSON objects before sorting:")
+pprint.pprint(employee_list)
+
+# Declare function to return the sorted data based on name
+def sort_by_key(list):
+    return list['name']
+
+# Print the sorted JSON list based on the name key
+print("\nArray of JSON objects after sorting:")
+pprint.pprint(sorted(employee_list, key=sort_by_key))
+"""
